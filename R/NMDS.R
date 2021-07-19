4b9a935ac8a9f8949fd83bc25d8c8faf451310d0
CHKim5 <demizor3@gmail.com>
2021-07-19 15:17:52 +0900

    Update NMDS.R

--- R/NMDS.R
+++ R/NMDS.R
@@ -6,6 +6,9 @@
 #' @param legend_position legend position "none","left","right","bottom","top"
 #' @param fig_width figure size
 #' @param fig_height figure size
+#' @param names whether the names are to be indicated T,F
+#' @param labsize label size
+#' @param dotsize dot size
 #'
 #' @return NMDS plot
 #' @export
@@ -18,7 +21,10 @@ NMDS<-function(Data,
                color = c("#FF3300","#660099","#FFCC00","#99CC00","#0066CC","#FF6600"),
                legend_position = "bottom",
                fig_width = 24,
-               fig_height = 20
+               fig_height = 20,
+               dotsize = 3,
+               names = F,
+               labsize = 3
                ){
   ifelse(!dir.exists(file.path(getwd(), "NMDS")), dir.create(file.path(getwd(), "NMDS")), FALSE)
   colnames(Data) <- Data[1, ]
@@ -38,9 +44,9 @@ NMDS<-function(Data,
                      method = method,
                      by = NULL,
                      data = x_y_coord_G)[["Pr(>F)"]][1] #Model P_Value
-  ggplot2::ggplot() +
-    ggplot2::geom_point(data = x_y_coord_G, ggplot2::aes(x = NMDS1, y = NMDS2,
-                                       color = Group), size = 3)+
+  sem_res<-ggplot2::ggplot(data = x_y_coord_G, ggplot2::aes(x = NMDS1, y = NMDS2,
+                                                   color = Group,label=rownames(x_y_coord_G))) +
+    ggplot2::geom_point(size = dotsize)+
     ggplot2::theme_minimal() +
     ggplot2::scale_color_manual(values = color)+
     ggplot2::theme(plot.title=ggplot2::element_text(size=10,
@@ -55,10 +61,13 @@ NMDS<-function(Data,
                                                       size=10),  # X axis title
                    axis.title.y=ggplot2::element_text(size=10),  # Y axis title
                    legend.position = legend_position)+
-    ggplot2::stat_ellipse(ggplot2::aes(x=x_y_coord_G$NMDS1,
-                                       y=x_y_coord_G$NMDS2,color=x_y_coord_G$Group),
+    ggplot2::stat_ellipse(ggplot2::aes(x=NMDS1,
+                                       y=NMDS2,color=Group),
                           type = "norm")+
     ggplot2::ggtitle(paste(method,"index/distance, ","Stress Level : ",NMDS$stress,"Model_p_value",M_p_value))
+  if (names == T){
+    sem_res+ggrepel::geom_text_repel(vjust = 0, nudge_y = 0.5,size=labsize)}
+  else {sem_res}
   ggplot2::ggsave(paste(method,"NMDS.png",sep = "_"),
                   path=paste0(getwd(),"/NMDS"),
                   height = fig_height,
